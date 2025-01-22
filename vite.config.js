57c21b54ba8a727491ad828bf44e3923e31c3cc8
rangakuu <rangaku.trading.co@gmail.com>
2025-01-22 01:23:57 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wisdom/',
+  base: '/wisdom/', // Your GitHub Pages repository name
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
